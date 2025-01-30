b70e210d070960579918aa061b5441edbfc848d8
jiang <jiangyii1996@gmail.com>
2025-01-30 14:14:47 +0800

    setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div id="jiang">
+    <ul>
+      <li>a</li>
+      <li>b</li>
+      <li>c</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
